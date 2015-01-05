f5305edcc7631b054629cc54d27e473f658054c7
Ten Mutunhire <tmutunhire@gmail.com>
2015-01-05 20:31:30 +0000

    version2

--- app/scripts/contentscripts.coffee
+++ app/scripts/contentscripts.coffee
@@ -7,7 +7,7 @@ window.addEventListener "load", ->
 	html.setAttribute "ng-csp", ""
 
 	#viewport = document.getElementById "viewport"
-	viewport = document.getElementById ":26"
+	viewport = document.getElementsByClassName("Ykrj7b")[0]
 	viewport.setAttribute "ng-controller", "MainController"
 
 	# add a dummy controller
@@ -18,13 +18,12 @@ window.addEventListener "load", ->
 	myDirective.setAttribute "my-directive", ""
 	viewport.appendChild myDirective
 
-	project_tabs = '<div class="Wn" id=":26">
-<button class="gbqfba" aria-label="Google Search" id="gbqfba" name="btnK"><span id="gbqfsa">EMAIL</span></button>
+	project_tabs = '<span class="Ykrj7b">&nbsp<button class="gbqfba" aria-label="Google Search" id="gbqfba" name="btnK"><span id="gbqfsa">EMAIL</span></button>
 <button class="gbqfba" aria-label="Google Search" id="gbqfba" name="btnK"><span id="gbqfsa">TASKS</span></button>
 <button class="gbqfba" aria-label="Google Search" id="gbqfba" name="btnK"><span id="gbqfsa">FILES</span></button>
 <button class="gbqfba" aria-label="Google Search" id="gbqfba" name="btnK"><span id="gbqfsa">CALENDAR</span></button>
 <button class="gbqfba" aria-label="Google Search" id="gbqfba" name="btnK"><span id="gbqfsa">PROJECT INFO</span></button>
-<span id=":29" class="aw5"></span></div>'
+</span>'
 
 	template1 = "<div>Search with Bing</div>"
 
--- app/scripts/contentscripts.js
+++ app/scripts/contentscripts.js
@@ -6,13 +6,13 @@
     html = document.querySelector("html");
     html.setAttribute("ng-app", "");
     html.setAttribute("ng-csp", "");
-    viewport = document.getElementById(":26");
+    viewport = document.getElementsByClassName("Ykrj7b")[0];
     viewport.setAttribute("ng-controller", "MainController");
     app.controller("MainController", function($scope) {});
     myDirective = document.createElement("div");
     myDirective.setAttribute("my-directive", "");
     viewport.appendChild(myDirective);
-    project_tabs = '<div class="Wn" id=":26"> <button class="gbqfba" aria-label="Google Search" id="gbqfba" name="btnK"><span id="gbqfsa">EMAIL</span></button> <button class="gbqfba" aria-label="Google Search" id="gbqfba" name="btnK"><span id="gbqfsa">TASKS</span></button> <button class="gbqfba" aria-label="Google Search" id="gbqfba" name="btnK"><span id="gbqfsa">FILES</span></button> <button class="gbqfba" aria-label="Google Search" id="gbqfba" name="btnK"><span id="gbqfsa">CALENDAR</span></button> <button class="gbqfba" aria-label="Google Search" id="gbqfba" name="btnK"><span id="gbqfsa">PROJECT INFO</span></button> <span id=":29" class="aw5"></span></div>';
+    project_tabs = '<span class="Ykrj7b">&nbsp<button class="gbqfba" aria-label="Google Search" id="gbqfba" name="btnK"><span id="gbqfsa">EMAIL</span></button> <button class="gbqfba" aria-label="Google Search" id="gbqfba" name="btnK"><span id="gbqfsa">TASKS</span></button> <button class="gbqfba" aria-label="Google Search" id="gbqfba" name="btnK"><span id="gbqfsa">FILES</span></button> <button class="gbqfba" aria-label="Google Search" id="gbqfba" name="btnK"><span id="gbqfsa">CALENDAR</span></button> <button class="gbqfba" aria-label="Google Search" id="gbqfba" name="btnK"><span id="gbqfsa">PROJECT INFO</span></button> </span>';
     template1 = "<div>Search with Bing</div>";
     app.directive("myDirective", function() {
       return {
